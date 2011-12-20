543e323cc0e83296650bd6dbbb452e7fb52531ab
Alan Pies <alan.lee.pies@gmail.com>
2011-12-20 08:11:21 -0800

    first commit

---
+++ PayablesVoucher/Bill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace PayablesVoucher
+{
+    class Bill
+    {
+        public IEnumerable<LineItem> XmlBillParse(string billpath)
+        {
+            XElement bill = XElement.Parse(billpath);
+
+            foreach (XElement line in bill.Element("Breakdown_TotalCharges_Details").Elements("Breakdown_TotalCharges_Record"))
+            {
+                LineItem lineItem = new LineItem();
+                lineItem.Amount = Convert.ToDecimal(bill.Element("Breakdown_Total_Chrgs").Value);
+                lineItem.Description = bill.Element("Breakdown_TotalCharges_PhoneNumber").Value + bill.Element("Breakdown_TotalCharges_UserName").Value;
+                lineItem.Distribution = "";
+                yield return lineItem;
+            }
+
+
+        }
+    }
+}
---
+++ PayablesVoucher/LineItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PayablesVoucher
+{
+    class LineItem
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Distribution { get; set; }
+    }
+}
