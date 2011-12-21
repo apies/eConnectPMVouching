b9ecf7b94ff8a9c5dd8745047001a73eec346ec8
Alan Pies <alan.lee.pies@gmail.com>
2011-12-20 17:34:26 -0800

    GP Push mechanism is coming together

--- PayablesVoucher/Voucher.cs
+++ PayablesVoucher/Voucher.cs
@@ -4,61 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.Dynamics.GP.eConnect;
+using System.Xml;
 
 namespace PayablesVoucher
 {
     class Voucher
     {
-         
+
         public XElement MakeXmlVoucher(IEnumerable<LineItem> lineItems)
         {
-          
-           
-           
-     
-                   
-              Decimal totalAmount = (from lineItem in lineItems
-                                    select lineItem.Amount).Sum();
-                                    
-              
-      
-                      var xPayable = new XElement("eConnect",
-                         new XElement("PMTransactionType",
-                           new XElement("taPMTransactionInsert",
-                             new XElement("BACHNUMB", "ALP 11/10/2011"),
-                             new XElement("VCHNUMWK", "ALAN PIES AMEX 11/11/2011"),
-                             new XElement("VENDORID", "AME102"),
-                             new XElement("DOCNUMBR", "ALAN PIES AMEX 11/11/2011"),
-                             new XElement("DOCTYPE", "1"),
-                             new XElement("DOCAMNT", totalAmount),
-                             new XElement("CHRGAMNT", totalAmount),
-                             new XElement("DOCDATE", "10/17/2011"),
-                             new XElement("PRCHAMNT", totalAmount),
-                             new XElement("CREATEDIST", "0")), 
-                             new XElement("taPMDistribution_Items",
-                           from lineItem in lineItems
-                           select new XElement("taPMDistribution",
-                             new XElement("DOCTYPE", "1"),
-                             new XElement("VCHRNMBR", "ALAN PIES AMEX 11/11/2011"),
-                             new XElement("VENDORID", "AME102"),
-                             new XElement("DEBITAMT", lineItem.Amount),
-                             new XElement("DistRef", lineItem.Description),
-                             new XElement("CRDTAMT", "0"),
-                             new XElement("ACTNUMST", lineItem.Distribution)))));
+
+
+
+
+
+            Decimal totalAmount = (from lineItem in lineItems
+                                   select lineItem.Amount).Sum();
+
+
+
+            var xPayable = new XElement("eConnect",
+               new XElement("PMTransactionType",
+                 new XElement("taPMTransactionInsert",
+                   new XElement("BACHNUMB", "ALP 11/10/2011"),
+                   new XElement("VCHNUMWK", "ALAN PIES AMEX 11/11/2011"),
+                   new XElement("VENDORID", "AME102"),
+                   new XElement("DOCNUMBR", "ALAN PIES AMEX 11/11/2011"),
+                   new XElement("DOCTYPE", "1"),
+                   new XElement("DOCAMNT", totalAmount),
+                   new XElement("CHRGAMNT", totalAmount),
+                   new XElement("DOCDATE", "10/17/2011"),
+                   new XElement("PRCHAMNT", totalAmount),
+                   new XElement("CREATEDIST", "0")),
+                   new XElement("taPMDistribution_Items",
+                 from lineItem in lineItems
+                 select new XElement("taPMDistribution",
+                   new XElement("DOCTYPE", "1"),
+                   new XElement("VCHRNMBR", "ALAN PIES AMEX 11/11/2011"),
+                   new XElement("VENDORID", "AME102"),
+                   new XElement("DEBITAMT", lineItem.Amount),
+                   new XElement("DistRef", lineItem.Description),
+                   new XElement("CRDTAMT", "0"),
+                   new XElement("ACTNUMST", lineItem.Distribution)))));
             return xPayable;
 
-                
+
         }
 
-                
-        public void PushtoGP(XElement )
+
+        public void PushtoGP(XElement payablex)
         {
-        
+
             eConnectMethods e = new eConnectMethods();
-            string employeeUpdate = xemployeeUpdate.ToString();
+            string payableVoucher = payablex.ToString();
 
             // Specify the Microsoft Dynamics GP server and database in the connection string
-            string sConnectionString = @"data source=VM-GPD01;initial catalog=tmi;integrated security=SSPI;persist security info=False;packet size=4096";
+            string sConnectionString = @"data source=sc-gpd;initial catalog=tmi;integrated security=SSPI;persist security info=False;packet size=4096";
 
             // Create an XML Document object for the schema
             XmlDocument XsdDoc = new XmlDocument();
@@ -71,6 +72,7 @@ namespace PayablesVoucher
             string sXsdSchema = XsdDoc.OuterXml;
 
             // Pass in xsdSchema to validate against.
-            e.eConnect_EntryPoint(sConnectionString, EnumTypes.ConnectionStringType.SqlClient, employeeUpdate, EnumTypes.SchemaValidationType.XSD, sXsdSchema);
+            e.eConnect_EntryPoint(sConnectionString, EnumTypes.ConnectionStringType.SqlClient, payableVoucher, EnumTypes.SchemaValidationType.XSD, sXsdSchema);
+        }
     }
 }
